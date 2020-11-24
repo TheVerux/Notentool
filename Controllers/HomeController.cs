f0cb5e501d5fea3864988a37b081e03a73037801
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-11-24 08:20:53 +0100

    spell check

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Notentool.Models;
 
@@ -42,20 +43,63 @@ namespace Notentool.Controllers
             return View();
         }
 
-        [HttpGet]
-        public ActionResult FriendDetail()
-        {
-            return View();
-        }
-
         [HttpPost]
         public ActionResult ModulErstellen(Modul modul)
         {
+            //Fetching Skills Records in LIST Collection format.  
+            var semesters = (from a in _context.Semester orderby a.Title select a).ToList();
+
+            //Creating ViewBag named SkillListItem to used in VIEW.  
+            ViewBag.SemesterListItem = ToSelectListSemester(semesters);
             _context.Moduls.Add(modul);
             _context.SaveChanges();
             return View();
         }
 
+        [HttpPost]
+        public ActionResult NoteErstellen(Grade grade)
+        {
+            //Fetching Skills Records in LIST Collection format.  
+            var grades = (from a in _context.Grade orderby a.Title select a).ToList();
+
+            //Creating ViewBag named SkillListItem to used in VIEW.  
+            ViewBag.ModulListItem= ToSelectListModul(semesters);
+            _context.Moduls.Add(modul);
+            _context.SaveChanges();
+            return View();
+        }
+
+        [NonAction]
+        public SelectList ToSelectListSemester(List<_context.Semester> semesters)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            foreach (_context.Semester item in semesters)
+            {
+                list.Add(new SelectListItem()
+                { 
+                    Text = item.Title
+                });
+            }
+
+            return new SelectList(list, "Text");
+        }
+
+        [NonAction]
+        public SelectList ToSelectListModul(List<_context.Modul> moduls)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            foreach (_context.Modul item in moduls)
+            {
+                list.Add(new SelectListItem()
+                { 
+                    Text = item.Title
+                });
+            }
+
+            return new SelectList(list, "Text");
+        }
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
