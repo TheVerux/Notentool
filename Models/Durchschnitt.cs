751ce627d985b74bedf5dd146e82130342c9340a
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-12-01 08:50:05 +0100

    Naming and Method Calculate

---
+++ Models/Durchschnitt.cs
@@ -0,0 +1,21 @@
+﻿using Notentool.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notentool.Models
+{
+    public static class Durchschnitt
+    {
+        public static double CalculateForSemester(Semester semester)
+        {
+
+        }
+
+        public static double CalculateForModul(Modul modul)
+        {
+            
+        }
+    }
+}
