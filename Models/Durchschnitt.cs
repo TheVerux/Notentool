8a4fcb3d47596ebc6a24456fa0e6ff4dd79a0ed2
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-12-01 09:16:32 +0100

    Calculate done 50

--- Models/Durchschnitt.cs
+++ Models/Durchschnitt.cs
@@ -9,21 +9,33 @@ namespace Notentool.Models
     public static class Durchschnitt
     {
         public static double CalculateForSemester(Semester semester)
-        {
+        { 
             IEnumerable<Grade> grades = new List<Grade>();
-            foreach (var modul in semester.Moduls)
+            if (semester.Moduls != null)
             {
-                grades.ToList().AddRange(modul.Grades);
+                foreach (var modul in semester?.Moduls)
+                {
+                    if (modul.Grades != null) grades.ToList().AddRange(modul.Grades);
+                }
             }
-
-            var noten = grades.Select(g => g.Note).AsQueryable();
-            return Queryable.Average(noten);
+            if (grades.ToList().Count > 0)
+            {
+                var noten = grades.Select(g => g.Note).AsQueryable();
+                return Queryable.Average(noten);
+            }
+            return 0;
         }
 
         public static double CalculateForModul(Modul modul)
         {
-            var noten = modul.Grades.Select(g => g.Note).AsQueryable();
-            return Queryable.Average(noten);
+            IEnumerable<Grade> grades = new List<Grade>();
+            if (modul.Grades != null) grades.ToList().AddRange(modul.Grades);
+            if (grades.ToList().Count > 0)
+            {
+                var noten = grades.Select(g => g.Note).AsQueryable();
+                return Queryable.Average(noten);
+            }
+            return 0;
         }
     }
 }
