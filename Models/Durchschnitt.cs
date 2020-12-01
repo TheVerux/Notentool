899b2ed523f82dc2f68400733432fd7161cc9b77
vmadmin <vmadmin@BMWP1>
2020-12-01 08:57:50 +0100

    Added average calculator

--- Models/Durchschnitt.cs
+++ Models/Durchschnitt.cs
@@ -10,12 +10,20 @@ namespace Notentool.Models
     {
         public static double CalculateForSemester(Semester semester)
         {
+            IEnumerable<Grade> grades = new List<Grade>();
+            foreach (var modul in semester.Moduls)
+            {
+                grades.ToList().AddRange(modul.Grades);
+            }
 
+            var noten = grades.Select(g => g.Note).AsQueryable();
+            return Queryable.Average(noten);
         }
 
         public static double CalculateForModul(Modul modul)
         {
-            
+            var noten = modul.Grades.Select(g => g.Note).AsQueryable();
+            return Queryable.Average(noten);
         }
     }
 }
